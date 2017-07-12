91dff1bc4f1531a3e5f7ed04063a28bb6415a4a9
Philip Zak <zakphi@gmail.com>
2017-07-12 09:34:00 -0400

    added title, created card constructor

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
-});
+  console.log('main.js connected')
+  
+  let card = function(faceVal, suit, numVal){
+    this.faceVal = faceVal
+    this.suit = suit
+    this.numVal = numVal
+  }
+})
